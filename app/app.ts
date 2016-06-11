03d086be9b3ad7d1f55d14b0197e0409fab5ab6d
colinjlacy <colin@webcake.co>
2016-06-11 07:31:41 -0500

    [local-firebase] formats lines in app.ts

--- app/app.ts
+++ app/app.ts
@@ -5,26 +5,26 @@ import * as firebase from 'firebase';
 
 
 @App({
-  template: '<ion-nav [root]="rootPage"></ion-nav>',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+    template: '<ion-nav [root]="rootPage"></ion-nav>',
+    config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
-  rootPage: any = TabsPage;
+    rootPage: any = TabsPage;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-        // Initialize Firebase
-        var config = {
-            apiKey: "AIzaSyD44soNIK81xzRjQXZ2Lmlb03CBIx4zVY4",
-            authDomain: "fir-app-f088c.firebaseapp.com",
-            databaseURL: "https://fir-app-f088c.firebaseio.com",
-            storageBucket: "fir-app-f088c.appspot.com"
-        };
-        firebase.initializeApp(config);
+    constructor(platform: Platform) {
+        platform.ready().then(() => {
+            // Initialize Firebase
+            var config = {
+                apiKey: "AIzaSyD44soNIK81xzRjQXZ2Lmlb03CBIx4zVY4",
+                authDomain: "fir-app-f088c.firebaseapp.com",
+                databaseURL: "https://fir-app-f088c.firebaseio.com",
+                storageBucket: "fir-app-f088c.appspot.com"
+            };
+            firebase.initializeApp(config);
 
-        // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
-  }
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+        });
+    }
 }
